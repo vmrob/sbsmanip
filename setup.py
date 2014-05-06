9aebc832766e722af96191ae7fd1b60c7a298d7d
Victor Robertson <victor.robertson.iv@gmail.com>
2014-05-06 02:39:02 -0500

    Add count_objects functionality to segc
    This patch adds the functionality brought from count_objects to the
    segc binary and sets up the structure for the remainder of the
    project.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ config = {
     'description': 'SEGarbageCollect is a set of utilities designed to help '
                    'clean a Space Engineers savefile for performance reasons',
     'author': 'Victor Robertson',
+    'license': 'MIT License',
     'url': 'https://github.com/vmrob/SEGarbageCollect',
     'download_url': 'https://github.com/vmrob/SEGarbageCollect/tarball/master',
     'author_email': 'victor.robertson.iv@gmail.com',
@@ -14,7 +15,7 @@ config = {
     'install_requires': ['nose'],
     'packages': ['SEGarbageCollect'],
     'scripts': [
-        'bin/count_objects.py',
+        'bin/segc',
         'bin/plot_sector.py',
         'bin/remove_debris.py',
         'bin/remove_objects.py',

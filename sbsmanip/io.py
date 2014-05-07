e66a4cd7ad2dee6be9b1e7d16407d3fee13794df
Victor Robertson <victor.robertson.iv@gmail.com>
2014-05-07 13:20:14 -0500

    Add Entity interface
    This patch adds an Entity class that can be used to query entity nodes
    found in the xml tree and tests for those accessors.

--- sbsmanip/io.py
+++ sbsmanip/io.py
@@ -1,13 +1,9 @@
 import xml.etree.ElementTree as ET
 
-
-class _SESchema:
-
-    sector_root = 'SectorObjects'
-    entity_base = 'MyObjectBuilder_EntityBase'
+import sector
 
 
-class _XMLFile:
+class _XMLFile(object):
 
     def __init__(self, filename):
         self._tree = ET.parse(filename)
@@ -16,6 +12,6 @@ class _XMLFile:
 
 class SBSFile(_XMLFile):
 
-    def entity_count(self):
-        return len(self._root.find(
-            _SESchema.sector_root).findall(_SESchema.entity_base))
+    def __init__(self, filename):
+        super(SBSFile, self).__init__(filename)
+        self.sector = sector.Sector(self._root)
--- tests/sbsmanip_tests.py
+++ tests/sbsmanip_tests.py
@@ -1,6 +1,32 @@
 import sbsmanip.io
+import sbsmanip.sector
 
 
 def test_count():
     savefile = sbsmanip.io.SBSFile('tests/support/SANDBOX_0_0_0_.sbs')
-    assert savefile.total_objects() == 102
+    assert savefile.sector.entity_count() == 102
+
+
+def test_sector_entities_generator():
+    savefile = sbsmanip.io.SBSFile('tests/support/SANDBOX_0_0_0_.sbs')
+    sector = savefile.sector
+
+    entities = list(sector.entities())
+    for e in sector.entities():
+        assert hasattr(e, 'id')
+        assert type(e.position.x) is float
+        assert type(e.position.y) is float
+        assert type(e.position.z) is float
+    assert len(entities) == 102
+
+
+def test_sector_single_entity():
+    savefile = sbsmanip.io.SBSFile('tests/support/SANDBOX_0_0_0_.sbs')
+    e = savefile.sector.entity('1066294968834912967')
+
+    assert e.id == '1066294968834912967'
+
+    assert e.position.x == -1118.05237
+    assert e.position.y == 6167.3833
+    assert e.position.z == 6817.74072
+    
